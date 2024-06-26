ff5bea19f56c0dfd47b676651d8f280ea4de52e7
miteshsingla <miteshhsingla@gmail.com>
2024-06-27 00:12:52 +0530

    get started screen

---
+++ app/src/main/java/com/timeit/UserDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.timeit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.timeit.app.R
+
+class UserDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_details)
+    }
+}
