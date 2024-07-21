fdeea124bbfad91f9f13acd7f41375fc7e4fb50e
miteshsingla <miteshhsingla@gmail.com>
2024-07-21 22:19:52 +0530

    habits list shown

--- app/src/main/java/com/cuebit/io/Fragments/HomeFragment.kt
+++ app/src/main/java/com/cuebit/io/Fragments/HomeFragment.kt
@@ -16,7 +16,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
